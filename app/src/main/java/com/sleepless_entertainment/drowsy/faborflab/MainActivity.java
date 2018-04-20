c54a653f8efc351b4afc641a9089fe815175d381
Dalton Pierce <dnnp2011@gmail.com>
2018-04-20 00:57:17 -0400

    Finished main activity layout, started detail activity

--- app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/MainActivity.java
+++ app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/MainActivity.java
@@ -1,13 +1,32 @@
 package com.sleepless_entertainment.drowsy.faborflab;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    //region Button Listeners
+    public void onClickWeightlifting(View view) {
+        assert view != null;
+        System.out.println("Click Weightlifting");
+    }
+
+    public void onClickYoga(View view) {
+        System.out.println("Click Yoga");
+    }
+
+    public void onClickCardio(View view) {
+        System.out.println("Click Cardio");
+    }
+    //endregion
 }
