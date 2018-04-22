b9687ea6040c06a35e9658669803165817a282b1
dnnp2011 <dnnp2011@gmail.com>
2018-04-22 00:22:08 -0400

    Completed simple logic implementaion for DetailActivity

--- app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/DetailActivity.java
+++ app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/DetailActivity.java
@@ -3,23 +3,150 @@ package com.sleepless_entertainment.drowsy.faborflab;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import java.util.HashMap;
 
 public class DetailActivity extends Activity {
+
+    private static final int NUM_CHECKBOX = 5;
+    private static String CurrentExerciseBG;
+
+    private View weightInclude;
+    private View yogaInclude;
+    private View cardioInclude;
+
+    private LinearLayout weightLinearLayout;
+    private LinearLayout yogaLinearLayout;
+    private LinearLayout cardioLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        CurrentExerciseBG = getIntent().getStringExtra("exercise.background.type");
+
+        weightInclude = findViewById(R.id.weightLiftingBG);
+        yogaInclude = findViewById(R.id.yogaBG);
+        cardioInclude = findViewById(R.id.cardioBG);
+
+        weightLinearLayout = weightInclude.findViewById(R.id.weightLinearLayout);
+        yogaLinearLayout = yogaInclude.findViewById(R.id.yogaLinearLayout);
+        cardioLinearLayout = cardioInclude.findViewById(R.id.cardioLinearLayout);
+
+        loadBackground(CurrentExerciseBG);
+
+//        TODO: Have up arrow pop up an exercise selection UI, to avoid reliance on NEXT button
+//        TODO: Implement top menu button
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setExerciseCompletion(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setExerciseCompletion(null);
+    }
+
+    //region Helper Methods
+    private void loadBackground(String targetBackground) {
+        assert weightInclude != null && yogaInclude != null && cardioInclude != null;
+
+        switch(targetBackground) {
+            case MainActivity.EXERCISE_WEIGHTS:
+                weightInclude.setVisibility(View.VISIBLE);
+                yogaInclude.setVisibility(View.GONE);
+                cardioInclude.setVisibility(View.GONE);
+                break;
+            case MainActivity.EXERCISE_YOGA:
+                weightInclude.setVisibility(View.GONE);
+                yogaInclude.setVisibility(View.VISIBLE);
+                cardioInclude.setVisibility(View.GONE);
+                break;
+            case MainActivity.EXERCISE_CARDIO:
+                weightInclude.setVisibility(View.GONE);
+                yogaInclude.setVisibility(View.GONE);
+                cardioInclude.setVisibility(View.VISIBLE);
+                break;
+            default:
+                System.out.println("ERROR-> DetailActivity.onCreate() --> Unable to resolve intended exercise background...");
+                break;
+        }
+
+        CurrentExerciseBG = targetBackground;
+    }
+
+    private String getNextBackground(String currentBackground) {
+        switch (currentBackground) {
+            case MainActivity.EXERCISE_WEIGHTS:
+                return MainActivity.EXERCISE_YOGA;
+            case MainActivity.EXERCISE_YOGA:
+                return MainActivity.EXERCISE_CARDIO;
+            case MainActivity.EXERCISE_CARDIO:
+                return MainActivity.EXERCISE_WEIGHTS;
+            default:
+                System.out.println("ERROR-> DetailActivity.getNextBackgroung() --> Unable to resolve current exercise background...");
+                return MainActivity.EXERCISE_WEIGHTS;
+        }
+    }
+
+    /**
+     * Sets the percentage of exercise completion based on how many checkboxes are ticked in each category
+     * @param view An optional parameter too allow calling from onClick listener when checkboxes are ticked
+     */
+    public void setExerciseCompletion(@Nullable View view) {
+        int weightChecked = 0;
+        int yogaChecked = 0;
+        int cardioChecked = 0;
+
+        for (int i = 0; i < weightLinearLayout.getChildCount(); i++) {
+            if (weightLinearLayout.getChildAt(i).getClass() != CheckBox.class)
+                continue;
+
+//            If this class check doesn't work, I can use a try block to catch when the cast fails
+            CheckBox child = (CheckBox) weightLinearLayout.getChildAt(i);
+            if (child.isChecked())
+                weightChecked += 1;
+        }
+        for (int i = 0; i < yogaLinearLayout.getChildCount(); i++) {
+            if (yogaLinearLayout.getChildAt(i).getClass() != CheckBox.class)
+                continue;
+
+            CheckBox child = (CheckBox) yogaLinearLayout.getChildAt(i);
+            if (child.isChecked())
+                yogaChecked += 1;
+        }
+        for (int i = 0; i < cardioLinearLayout.getChildCount(); i++) {
+            if (cardioLinearLayout.getChildAt(i).getClass() != CheckBox.class)
+                continue;
+
+            CheckBox child = (CheckBox) cardioLinearLayout.getChildAt(i);
+            if (child.isChecked())
+                cardioChecked += 1;
+        }
+
+        MainActivity.WeightCompletion = (int) (((float)weightChecked / (float)NUM_CHECKBOX) * 100f);
+        MainActivity.YogaCompletion = (int) (((float)yogaChecked / (float)NUM_CHECKBOX) * 100f);
+        MainActivity.CardioCompletion = (int) (((float)cardioChecked / (float)NUM_CHECKBOX) * 100f);
+    }
+    //endregion
+
+    //region Button Listeners
     public void onClickBack(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
 
     public void onClickNext(View view) {
-
+        loadBackground(CurrentExerciseBG = getNextBackground(CurrentExerciseBG));
     }
 
     public void onClickMenu(View view) {
@@ -29,4 +156,5 @@ public class DetailActivity extends Activity {
     public void onClickUp(View view) {
 
     }
+    //endregion
 }
--- app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/MainActivity.java
+++ app/src/main/java/com/sleepless_entertainment/drowsy/faborflab/MainActivity.java
@@ -4,35 +4,67 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    public static final String EXTRA_ITEM_TITLE = "exercise.background.type";
 
     public static final String EXERCISE_WEIGHTS = "Weight Lifting";
     public static final String EXERCISE_YOGA = "Yoga";
     public static final String EXERCISE_CARDIO = "Cardio";
+
+    public static Integer WeightCompletion = 0;
+    public static Integer YogaCompletion = 0;
+    public static Integer CardioCompletion = 0;
+
+    private TextView weightCompletionView;
+    private TextView yogaCompletionView;
+    private TextView cardioCompletionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        weightCompletionView = findViewById(R.id.w_percentageCompleteView);
+        yogaCompletionView = findViewById(R.id.y_percentageCompleteView);
+        cardioCompletionView = findViewById(R.id.c_percentageCompleteView);
 
+//        TODO: Deserialize completion amounts and checkboxes from disk
     }
 
-    //region Button Listeners
-    public void onClickWeightlifting(View view) {
-        assert view != null;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setCompletionLabels();
+    }
+
+    //region Helper Methods
+    private void loadDetailActivity(String exercise) {
         Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        intent.putExtra(EXTRA_ITEM_TITLE, exercise);
         startActivity(intent);
     }
 
+    private void setCompletionLabels() {
+        weightCompletionView.setText(String.format("%d%%", WeightCompletion));
+        yogaCompletionView.setText(String.format("%d%%", YogaCompletion));
+        cardioCompletionView.setText(String.format("%d%%", CardioCompletion));
+    }
+    //endregion
+
+    //region Button Listeners
+    public void onClickWeightlifting(View view) {
+        loadDetailActivity(EXERCISE_WEIGHTS);
+    }
+
     public void onClickYoga(View view) {
-        System.out.println("Click Yoga");
+        loadDetailActivity(EXERCISE_YOGA);
     }
 
     public void onClickCardio(View view) {
-        System.out.println("Click Cardio");
+        loadDetailActivity(EXERCISE_CARDIO);
     }
     //endregion
 }
